0af33c00e0a3ed3dc5e2acee2d22fb6dae345442
fabian-gubler <fabian.gubler@hotmail.com>
2021-11-03 17:05:30 +0100

    update

--- nvim/lua/config/opt.lua
+++ nvim/lua/config/opt.lua
@@ -11,7 +11,22 @@ g.EasyMotion_keys='arstdhneioqwfpluygmbjzxcvk1234567890,'
 
 -- Options
 o.relativenumber = true
+o.swapfile = false
 o.ignorecase = true
 o.termguicolors = true
-o.undodir = home .. '/.config/lvim/undodir'
+o.hidden = true
+o.smartcase = true
+o.undodir = home .. '/.local/share/nvim/undodir'
+o.colorcolumn = "80"
 o.clipboard = "unnamedplus"
+o.undofile = true
+o.smartindent = true
+o.wrap = false
+
+-- Don't know yet
+local indent = 4
+
+o.shiftwidth = indent
+o.tabstop = indent
+o.softtabstop = indent
+o.expandtab = false
