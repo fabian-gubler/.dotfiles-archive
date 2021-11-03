beae8003266f10648b22df10fbc8041c66e74733
fabian-gubler <fabian.gubler@hotmail.com>
2021-11-03 14:58:23 +0100

    update

--- nvim/lua/config/colemak.lua
+++ nvim/lua/config/colemak.lua
@@ -5,49 +5,47 @@ local map = vim.api.nvim_set_keymap
 local opt = { noremap = true }
 
 -- Navigation                 [Key]   [Action]     [Mode]
-map('', 'm', 'h', opt)  --    m       left         any
-map('', 'i', 'l', opt)  --    i       right        any
-map('', 'n', 'j', opt)  --    j       down         any
-map('', 'e', 'k', opt)  --    k       up           any
+map('', 'm', 'h', opt)    --    m       left         any
+map('', 'i', 'l', opt)    --    i       right        any
+map('', 'n', 'j', opt)    --    j       down         any
+map('', 'e', 'k', opt)    --    k       up           any
 
-map('', 'l', 'b', opt)  --    l       back         any
-map('', 'L', 'B', opt)  --    L       back         any
-map('', 'y', 'w', opt)  --    y       forward      any
-map('', 'Y', 'W', opt)  --    Y       forward      any
-map('', 'u', 'e', opt)  --    u       end          any
-map('', 'U', 'E', opt)  --    U       end          any
+map('', 'l', 'b', opt)    --    l       back         any
+map('', 'L', 'B', opt)    --    L       back         any
+map('', 'y', 'w', opt)    --    y       forward      any
+map('', 'Y', 'W', opt)    --    Y       forward      any
+map('', 'u', 'e', opt)    --    u       end          any
+map('', 'U', 'E', opt)    --    U       end          any
 
-map('', 'a', 'v', opt)  --    a       visual       any
-map('', 'A', 'V', opt)  --    A       visual       any
-map('', 'p', 't', opt)  --    p       find         any
-map('', 'P', 'T', opt)  --    P       find         any
+map('', 'a', 'v', opt)    --    a       visual       any
+map('', 'A', 'V', opt)    --    A       visual       any
+map('', 'p', 't', opt)    --    p       find         any
+map('', 'P', 'T', opt)    --    P       find         any
 
 -- Operation
-map('n', 's', 'i', opt) --    s       insert       normal
-map('n', 'S', 'I', opt) --    S       insert       normal
-map('n', 't', 'a', opt) --    t       append       normal
-map('n', 'T', 'A', opt) --    T       append       normal
-map('n', 'w', 'c', opt) --    w       change       normal
-map('x', 'w', 'c', opt) --    w       change       visual
+map('n', 's', 'i', opt)   --    s       insert       normal
+map('n', 'S', 'I', opt)   --    S       insert       normal
+map('n', 't', 'a', opt)   --    t       append       normal
+map('n', 'T', 'A', opt)   --    T       append       normal
+map('n', 'w', 'c', opt)   --    w       change       normal
+map('x', 'w', 'c', opt)   --    w       change       visual
 
-map('x', 'x', 'd', opt) --    x       cut          visual
-map('', 'c', 'y', opt)  --    c       copy         any
-map('n', 'C', 'yy', opt)--    C       copy         normal
-map('x', 'C', 'y', opt) --    C       copy         visual
-map('', 'v', 'p', opt)  --    v       paste        any
-map('', 'V', 'P', opt)  --    v       paste        any
+map('x', 'x', 'd', opt)   --    x       cut          visual
+map('', 'c', 'y', opt)    --    c       copy         any
+map('n', 'C', 'yy', opt)  --    C       copy         normal
+map('x', 'C', 'y', opt)   --    C       copy         visual
+map('', 'v', 'p', opt)    --    v       paste        any
+map('', 'V', 'P', opt)    --    v       paste        any
 
 -- MISC
-map('v', 'b', 'u', opt) --    b       case         visual & select
-map('v', 'B', 'U', opt) --    B       case         visual & select
-map('n', 'b', 'z', opt) --    b       fold         normal
-map('n', 'z', 'u', opt) --    z       undo         normal
-map('x', 'z', ':<C-U>undo<cr>', opt)  --           visual
-map('n', 'Z', '<C-R>', opt)           --           normal
-map('x', 'Z', ':<C-U>redo<cr>', opt)  --           visual
-
--- text objects (dip -> drp)
-map('o', 'r', 'i', opt)               --           operator pending
+map('v', 'b', 'u', opt)   --    b       case         visual & select
+map('v', 'B', 'U', opt)   --    B       case         visual & select
+map('n', 'b', 'z', opt)   --    b       fold         normal
+map('n', 'z', 'u', opt)   --    z       undo         normal
+map('x', 'z', ':<C-U>undo<cr>', opt) -- undo         visual
+map('n', 'Z', '<C-R>', opt)          -- redo         normal
+map('x', 'Z', ':<C-U>redo<cr>', opt) -- redo         visual
+map('o', 'r', 'i', opt)   --    dr*     inner        operator pending
 
 -- no idea how this works
 vim.cmd([[
