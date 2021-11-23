1653f556899f3bf965efe816c4f9a44e7d726c9f
fabian-gubler <fabian.gubler@hotmail.com>
2021-11-23 15:06:12 +0100

    update

--- ferdi/recipes/whatsapp/webview.js
+++ ferdi/recipes/whatsapp/webview.js
@@ -1,29 +1,31 @@
 const _path = _interopRequireDefault(require('path'));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
 
-window.addEventListener('beforeunload', async () => {
-  Ferdi.clearStorageData(['appcache', 'serviceworkers', 'cachestorage', 'websql', 'indexdb']);
-  Ferdi.releaseServiceWorkers();
-});
-
-module.exports = Ferdi => {
-  const getMessages = function getMessages() {
+module.exports = (Ferdi, settings) => {
+  const getMessages = () => {
     let count = 0;
     let indirectCount = 0;
 
-    const parentChatElem = Array.from(document.querySelectorAll('div[aria-label]'))
-                           .sort((a, b) => (a.offsetHeight < b.offsetHeight) ? 1 : -1)[0];
+    const parentChatElem = [
+      ...document.querySelectorAll('div[aria-label]'),
+    ].sort((a, b) => (a.offsetHeight < b.offsetHeight ? 1 : -1))[0];
     if (!parentChatElem) {
       return;
     }
 
     const unreadSpans = parentChatElem.querySelectorAll('span[aria-label]');
-    for (let i = 0; i < unreadSpans.length; i++) {
-      const unreadElem = unreadSpans[i];
+    for (const unreadElem of unreadSpans) {
       const countValue = Ferdi.safeParseInt(unreadElem.textContent);
       if (countValue > 0) {
-        if (!unreadElem.parentNode.previousSibling || unreadElem.parentNode.previousSibling.querySelectorAll('[data-icon=muted]').length === 0) {
+        if (
+          !unreadElem.parentNode.previousSibling ||
+          unreadElem.parentNode.previousSibling.querySelectorAll(
+            '[data-icon=muted]',
+          ).length === 0
+        ) {
           count += countValue;
         } else {
           indirectCount += countValue;
@@ -34,7 +36,31 @@ module.exports = Ferdi => {
     Ferdi.setBadge(count, indirectCount);
   };
 
-  Ferdi.loop(getMessages);
+  const getActiveDialogTitle = () => {
+    const element = document.querySelector('header .emoji-texttt');
+
+    Ferdi.setDialogTitle(element ? element.textContent : '');
+  };
+
+  const loopFunc = () => {
+    getMessages();
+    getActiveDialogTitle();
+  };
+
+  window.addEventListener('beforeunload', async () => {
+    Ferdi.clearStorageData(settings.id, {
+      storages: [
+        'appcache',
+        'serviceworkers',
+        'cachestorage',
+        'websql',
+        'indexdb',
+      ],
+    });
+    Ferdi.releaseServiceWorkers();
+  });
+
+  Ferdi.loop(loopFunc);
 
   Ferdi.injectCSS(_path.default.join(__dirname, 'service.css'));
 };
--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -4,7 +4,7 @@
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int gappx     = 10;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "SFMono:size=12" };
 static const char col_gray1[]       = "#252A34";
