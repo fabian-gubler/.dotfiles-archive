b7a7559e830ed0dfd7731da4d6cec177c0b37120
fabian-gubler <fabian.gubler@hotmail.com>
2021-11-25 13:00:01 +0100

    update

--- scripts/bash/server.sh
+++ scripts/bash/server.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 session="server"
 cwd=$(pwd)
-
 tmux new-session -d -s $session
 
 window=1
 tmux rename-window -t $session:$window 'live-server'
 tmux send-keys -t $session:$window "cd $cwd" C-m C-l
-tmux send-keys -t $session:$window 'live-server --port=6000' C-m
+tmux send-keys -t $session:$window 'live-server' C-m
 
 window=2
 tmux new-window -t $session:$window -n 'scss'
--- scripts/bash/tmux_shell.sh
+++ scripts/bash/tmux_shell.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-executable=$(find ~/.dotfiles/scripts/bash -maxdepth 1 -perm -111 -type f | fzf)
+# executable=$(find ~/.dotfiles/scripts/bash -maxdepth 1 -perm -111 -type f | fzf)
+path=~/.dotfiles/scripts/tmux
+
+cd $path
+executable=$(cd $path && fzf --reverse)
 $executable
