f563992ca3ad76fe752073328556f6af8edc7e29
fabian-gubler <fabian.gubler@hotmail.com>
2021-11-25 14:00:01 +0100

    update

--- scripts/bash/tmux_shell.sh
+++ scripts/bash/tmux_shell.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# executable=$(find ~/.dotfiles/scripts/bash -maxdepth 1 -perm -111 -type f | fzf)
 path=~/.dotfiles/scripts/tmux
 
 cd $path
-executable=$(cd $path && fzf --reverse)
-$executable
+./$(cd $path && fzf --reverse)
