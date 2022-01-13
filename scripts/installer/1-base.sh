09ae0f4c40eb92ef1e4a30c7f8bced3af9ae68e7
fabian-gubler <fabian.gubler@hotmail.com>
2022-01-13 11:30:28 +0100

    update

--- scripts/installer/1-base.sh
+++ scripts/installer/1-base.sh
@@ -1,1 +1,12 @@
-sudo apt install xorg
+# necessary packages
+sudo apt install make gcc libx11-dev libxft-dev libxinerama-dev xorg git
+
+# clone repo
+git clone https://github.com/fabian-gubler/.dotfiles.git
+
+# install dwm
+cd ~/.dotfiles/suckless/dwm
+sudo make clean install
+
+# startup
+ln -s ~/.dotfiles/scripts/installer/files/.xinitrc ~/.xinitrc
