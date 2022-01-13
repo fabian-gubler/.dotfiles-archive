f5b74b5ac59de83b69466fe638a270d382e2f1b3
fabian-gubler <fabian.gubler@hotmail.com>
2022-01-13 12:53:24 +0100

    manual update

--- installer/2-packages.sh
+++ installer/2-packages.sh
@@ -1,49 +1,49 @@
 PACKAGES=(
-    # VIRTUAL MACHINES --------------------------------------------------------
+	# VIRTUAL MACHINES --------------------------------------------------------
 	'qemu-system'
 	'libvirt-daemon-system'
 	'virt-manager'
 
-    # SYSTEM ------------------------------------------------------------------
+	# SYSTEM ------------------------------------------------------------------
 	'firmware-iwlwifi'		      # Bluetooth
 	'fzf'						  # Fuzzy finder
 	'redshift'					  # Night filter
-    'adwaita-qt'                  # Adwaita dark
+	'adwaita-qt'                  # Adwaita dark
 	'qt5ct'                       # Qt theming
 	'xclip'					      # Clipboard
 	'openvpn'					  # VPN client
 	'arandr'				      # xrandr frontend
 
-    # COMMAND LINE  -----------------------------------------------------------
+	# COMMAND LINE  -----------------------------------------------------------
 	'golang'					  # Programming Language
-    'python3-pip'                 # Python Package Manager
-    'zsh'                         # Shell
-    'tmux'                        # Terminal Multiplexer
-    'git'                         # Version control
-    'exa'                         # Better ls
-    'wget'                        # Download web content
+	'python3-pip'                 # Python Package Manager
+	'zsh'                         # Shell
+	'tmux'                        # Terminal Multiplexer
+	'git'                         # Version control
+	'exa'                         # Better ls
+	'wget'                        # Download web content
 	'curl'						  # ""
-    'ranger'                      # File explorer
-    'devour'                      # Swallow programs
-    'unzip'                       # Unzip directories
+	'ranger'                      # File explorer
+	'devour'                      # Swallow programs
+	'unzip'                       # Unzip directories
 
-    # APPLICATIONS ------------------------------------------------------------
-    'qbittorrent'                 # Torrent client
+	# APPLICATIONS ------------------------------------------------------------
+	'qbittorrent'                 # Torrent client
 	'xsane'                       # Scanning
-    'unclutter'                   # Mouse timeout
-    'flameshot'                   # Screenshot tool
-    'timeshift'                   # Backup snapshots
-    'baobab'                      # Disk space
-    'firefox-esr'                 # Web browser
-    'anki'                        # Flashcard app
-    'zathura'                     # PDF viewer
+	'unclutter'                   # Mouse timeout
+	'flameshot'                   # Screenshot tool
+	'timeshift'                   # Backup snapshots
+	'baobab'                      # Disk space
+	'firefox-esr'                 # Web browser
+	'anki'                        # Flashcard app
+	'zathura'                     # PDF viewer
 
-    # MEDIA -------------------------------------------------------------------
-    'bluez'                       # Bluetooth protocol
-    'pipewire'                    # Audio
-    'pavucontrol'                 # Audio Control
-    'sxiv'                        # Image viewer
-    'mpv'                         # Media player
+	# MEDIA -------------------------------------------------------------------
+	'bluez'                       # Bluetooth protocol
+	'pipewire'                    # Audio
+	'pavucontrol'                 # Audio Control
+	'sxiv'                        # Image viewer
+	'mpv'                         # Media player
 )
 
 sudo apt update
