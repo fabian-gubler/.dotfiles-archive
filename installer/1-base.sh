1c53a6df7c27c76f8f5cfdb7d09ee9ccdfbbe91a
fabian-gubler <fabian.gubler@hotmail.com>
2022-01-13 11:44:20 +0100

    update

--- installer/1-base.sh
+++ installer/1-base.sh
@@ -1,9 +1,14 @@
 # necessary packages
 sudo apt install make gcc libx11-dev libxft-dev libxinerama-dev xorg
 
-# install dwm
+# dwm
 cd ~/.dotfiles/suckless/dwm
 sudo make clean install
 
+# dmenu
+cd ~/.dotfiles/suckless/dmenu
+sudo make clean install
+
 # startup
-ln -s ~/.dotfiles/scripts/installer/files/.xinitrc ~/.xinitrc
+ln -s ~/.dotfiles/installer/xorg/.xinitrc ~/.xinitrc
+ln -s ~/.dotfiles/installer/xorg/.xprofile ~/.xprofile
