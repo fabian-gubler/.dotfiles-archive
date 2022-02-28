ca60de687a41c99202a2b65a6e5a80890a7649e8
fabian-gubler <fabian.gubler@hotmail.com>
2022-02-28 15:00:01 +0100

    automated update

--- installer/3-config.sh
+++ installer/3-config.sh
@@ -1,6 +1,29 @@
-# ------------------------------------------------------
-# --- PACKAGES -----------------------------------------
-# ------------------------------------------------------
+# -----------------------------------------------------------------------------
+# NODE PACKAGES
+# -----------------------------------------------------------------------------
+
+# Nodejs v17 | https://github.com/nodesource/distributions/blob/master/README.md
+sudo curl -fsSL https://deb.nodesource.com/setup_17.x | sudo bash -
+sudo apt install -y nodejs
+
+# Node Packages
+NODE=(
+	'create-react-app'			# React application
+	'express-generator'			# Express application
+	'tailwindcss'				# Styling
+)
+
+for PKG in "${NODE[@]}"; do
+    echo "INSTALLING: $PKG"
+    sudo sudo npm install -g "$PKG"
+done
+
+# -----------------------------------------------------------------------------
+# MISC PACKAGES
+# -----------------------------------------------------------------------------
+
+# Gotop
+go get github.com/cjbassi/gotop
 
 # Typora | https://support.typora.io/Typora-on-Linux/
 wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
@@ -18,16 +41,16 @@ sudo apt update && sudo apt install signal-desktop
 cargo install alacritty
 
 # Network manager
-git clone https://github.com/firecat53/networkmanager-dmenu ~/.local/bin/networkmanager-dmenu
+git clone https://github.com/firecat53/networkmanager-dmenu $HOME/.local/bin/networkmanager-dmenu
 
 # Dragon
-git clone https://github.com/mwh/dragon ~/Downloads/dragon
-cd ~/Downloads/dragon
+git clone https://github.com/mwh/dragon /tmp/dragon
+cd /tmp/dragon
 make
 
 # Devour
-git clone https://github.com/salman-abedin/devour.git ~/Downloads/devour
-cd ~/Downloads/devour
+git clone https://github.com/salman-abedin/devour.git /tmp/devour
+cd /tmp/devour
 sudo make install
 
 # Nextcloud
@@ -36,17 +59,9 @@ sudo apt install nextcloud-desktop
 # ZSA wally cli
 # ???
 
-# Printer | Might require: sudo apt install hplip
-# mkdir -p ~/HP-Laser
-# cd ~/HP-Laser
-# wget https://ftp.hp.com/pub/softlib/software13/printers/CLP150/uld-hp_V1.00.39.12_00.15.tar.gz
-# tar -xf  uld-hp_V1.00.39.12_00.15.tar.gz
-# cd ~/HP-Laser/uld
-# sudo ./install-printer.sh
-
-# Gotop | move to secrets
-# 	https://github.com/xxxserxxx/gotop/releases
-# 	mv gotop ~/.local/bin
+# Only Office | https://www.onlyoffice.com/de/download-desktop.aspx?from=desktop
+wget -O /tmp/onlyoffice.deb https://download.onlyoffice.com/install/desktop/editors/linux/onlyoffice-desktopeditors_amd64.deb
+sudo apt install /tmp/onlyoffice.deb
 
 # VPN setup | https://protonvpn.com/support/linux-openvpn/
 # 1. network-manager-openvpn-gnome
@@ -68,23 +83,3 @@ virsh net-autostart default
 virsh net-start default
 
 # Pipewire | https://wiki.debian.org/PipeWire
-
-# ------------------------------------------------------
-# --- NPM ----------------------------------------------
-# ------------------------------------------------------
-
-# Nodejs v17 | https://github.com/nodesource/distributions/blob/master/README.md
-sudo curl -fsSL https://deb.nodesource.com/setup_17.x | sudo bash -
-sudo apt install -y nodejs
-
-# Node Packages
-NODE=(
-	'create-react-app'			# React application
-	'express-generator'			# Express application
-	'tailwindcss'				# Styling
-)
-
-for PKG in "${NODE[@]}"; do
-    echo "INSTALLING: $PKG"
-    sudo sudo npm install -g "$PKG"
-done
