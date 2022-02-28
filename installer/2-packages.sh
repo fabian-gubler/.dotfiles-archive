1e1a48f98a8e30da198bc5496dd70e88e2ad8fea
fabian-gubler <fabian.gubler@hotmail.com>
2022-02-28 19:00:01 +0100

    automated update

--- installer/2-packages.sh
+++ installer/2-packages.sh
@@ -45,6 +45,7 @@ PACKAGES=(
 	'firefox-esr'                 # Web browser
 	'anki'                        # Flashcard app
 	'zathura'                     # PDF viewer
+	'okular'					  # PDF Editor
 
 	# MEDIA -------------------------------------------------------------------
 	'bluez'                       # Bluetooth protocol
