6d8575a628aede5c32aa85fd9208c91a91ab60e1
Fabian Gubler <fabian.gubler@hotmail.com>
2022-05-03 10:57:53 +0200

    manual update

--- scripts/utils/battery-notifier.sh
+++ scripts/utils/battery-notifier.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Description:
-# script that sends a notification
+# Script that sends a notification
 # when battery is below 10%
-
-# Dependencies: 
-# - dunst
-# - acpi
-
 
 while true
 do
