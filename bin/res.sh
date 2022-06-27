a078dc2e53bb2231c39d69770c024c58ae6bc80e
Fabian Gubler <fabian.gubler@hotmail.com>
2022-06-27 14:37:45 +0200

    manual update

--- bin/res.sh
+++ bin/res.sh
@@ -7,5 +7,4 @@
 # xrandr --newmode "3440x1440" 419.50 3440 3696 4064 4688 1440 1453 1493 -hsync +vsync
 xrandr --newmode "3440x1440"  419.50  3440 3696 4064 4688  1440 1443 1453 1493 -hsync +vsync
 xrandr --addmode Virtual1 "3440x1440"
-
-# xrandr --output Virtual1 --mode "3440x1440"
+xrandr --output Virtual1 --mode "3440x1440"
