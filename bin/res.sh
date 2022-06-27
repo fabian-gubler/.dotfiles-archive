c0af24539c82a777e4474e4a4a48443a12807e3a
Fabian Gubler <fabian.gubler@hotmail.com>
2022-06-27 14:36:14 +0200

    manual update

--- bin/res.sh
+++ bin/res.sh
@@ -3,6 +3,9 @@
 # Source: https://superuser.com/questions/716795/how-to-adjust-screen-resolution
 # Get params with: cvt 3440 1440
 
-xrandr --newmode "3440x1440" 419.50 3440 3696 4064 4688 1440 1453 1493 -hsync +vsync
+
+# xrandr --newmode "3440x1440" 419.50 3440 3696 4064 4688 1440 1453 1493 -hsync +vsync
+xrandr --newmode "3440x1440"  419.50  3440 3696 4064 4688  1440 1443 1453 1493 -hsync +vsync
 xrandr --addmode Virtual1 "3440x1440"
-xrandr --output Virtual1 --mode "3440x1440"
+
+# xrandr --output Virtual1 --mode "3440x1440"
