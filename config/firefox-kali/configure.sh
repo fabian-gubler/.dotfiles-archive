9c6c666ce6f2d0563d3b82a43179b653fbab0650
Fabian Gubler <fabian.gubler@hotmail.com>
2022-07-01 06:10:01 +0200

    manual update

---
+++ config/firefox-kali/configure.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ID=pf0yzukt
+SRC=/home/fabian/nextcloud/apps/firefox
+DEST=/home/fabian/.mozilla/firefox/${ID}.main
+
+rm -rf $DEST/chrome
+rm -rf $DEST/extensions
+ln -s $SRC/chrome $DEST/chrome
+ln -s $SRC/extensions $DEST/extensions
+
+
+ln -sf $SRC/user.js $DEST/user.js
+ln -sf $SRC/extensions.json $DEST/extensions.json
+ln -sf $SRC/extension-settings.json $DEST/extension-settings.json
