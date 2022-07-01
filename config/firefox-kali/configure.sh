65a52b35d903536159e12bf24680f0120aa8aaa0
kali <kali*kali.com>
2022-07-01 06:51:58 +0200

    Initial Commit

--- config/firefox-kali/configure.sh
+++ config/firefox-kali/configure.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-ID=pf0yzukt
+ID=vfzimg4s
 SRC=/home/kali/.dotfiles/config/firefox-kali
-DEST=/home/fabian/.mozilla/firefox/${ID}.main
+DEST=/home/kali/.mozilla/firefox/${ID}.main
 
 rm -rf $DEST/chrome
 ln -s $SRC/chrome $DEST/chrome
 ln -sf $SRC/user.js $DEST/user.js
-ln -sf $SRC/extensions.json $DEST/extensiontension-settings.json
+ln -sf $SRC/extension-settings.json $DEST/extension-settings.json
 
-# rm -rf $DEST/extensions
-# ln -s $SRC/extensions $DEST/extensions
+rm -rf $DEST/extensions
+ln -s $SRC/extensions $DEST/extensions
 
+# Stop tracking
+git rm -r --cached $SRC
