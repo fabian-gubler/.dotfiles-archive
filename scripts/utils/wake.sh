9af253d265a6a87e314460133863903af57cbda1
Fabian Gubler <fabian.gubler@hotmail.com>
2022-07-18 12:34:26 +0200

    manual update

---
+++ scripts/utils/wake.sh
@@ -0,0 +1,11 @@
+# Read desired time
+TIME=$1
+
+# Check Speaker (2 Seconds)
+mpv --length=2 ~/nextcloud/music/resonance.opus
+
+# Alarm at time
+at $TIME << ENDMARKER
+pactl set-sink-volume @DEFAULT_SINK@ 50%
+mpv --loop-file=inf --start=0 ~/nextcloud/music/resonance.opus
+ENDMARKER
