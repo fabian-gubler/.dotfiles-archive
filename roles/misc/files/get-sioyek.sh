39cd104990fe2db48de78f68e9d1a22888739d1c
Fabian Gubler <fabian.gubler@hotmail.com>
2022-12-06 13:25:04 +0100

    automated update

--- roles/misc/files/get-sioyek.sh
+++ roles/misc/files/get-sioyek.sh
@@ -1,13 +1,15 @@
-sudo dnf install qt5-qtbase-devel qt5-qtbase-static qt5-qt3d-devel harfbuzz-devel
+# SOURCE: https://github.com/ahrm/sioyek#fedora
 
-SAVE_DIR=$HOME/.local/share
+# sudo dnf install qt5-qtbase-devel qt5-qtbase-static qt5-qt3d-devel harfbuzz-devel
 
-[ -d $SAVE_DIR/sioyek ] && echo "Exited: Directory already exists!" && exit
-
-mkdir $SAVE_DIR/sioyek
-cd $SAVE_DIR
-git clone --recursive https://github.com/ahrm/sioyek sioyek
-cd sioyek
-./build_linux.sh
-
-ln -s $HOME/.local/share/sioyek/build/sioyek $HOME/.local/bin/
+# SAVE_DIR=$HOME/.local/share
+#
+# [ -d $SAVE_DIR/sioyek ] && echo "Exited: Directory already exists!" && exit
+#
+# mkdir $SAVE_DIR/sioyek
+# cd $SAVE_DIR
+# git clone --recursive https://github.com/ahrm/sioyek sioyek
+# cd sioyek
+# ./build_linux.sh
+#
+# ln -s $HOME/.local/share/sioyek/build/sioyek $HOME/.local/bin/
